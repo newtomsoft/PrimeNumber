29868d6e6089072a9106f35f987e9f5a11e78304
Thomas Vuille <thomas@newtomsoft.fr>
2022-02-21 18:53:32 +0100

    using var

--- PrimeNumberApp/PrimeNumber.cs
+++ PrimeNumberApp/PrimeNumber.cs
@@ -4,7 +4,7 @@ public static class PrimeNumber
 {
     public static bool IsPrime(ulong number)
     {
-        ulong computeLimit = (ulong)Math.Sqrt(number);
+        var computeLimit = (ulong)Math.Sqrt(number);
         for (ulong numberTested = 2; numberTested <= computeLimit; numberTested++)
             if (number % numberTested == 0) return false;
 
@@ -14,7 +14,7 @@ public static class PrimeNumber
     public static IEnumerable<ulong> FromTo(ulong from, ulong to)
     {
         if (from < 2) from = 2;
-        for (ulong numberTested = from; numberTested <= to; numberTested++)
+        for (var numberTested = from; numberTested <= to; numberTested++)
             if (IsPrime(numberTested)) yield return numberTested;
     }
 }
